5c2c3959657da90a276eddb09adbd69ead5a7009
Thomas Gonzales <thomas.j.gonzales@gmail.com>
2017-06-28 17:15:39 -0500

    set up db

--- main.js
+++ main.js
@@ -1,35 +1,86 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
+const models = require('./models/')
 const app = express()
 
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mustache')
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(express.static('public'))
 
-var todos = []
-var done = []
 
-app.get('/', function(req, res){
-  res.render('index', {todos: todos, done})
-})
-
-app.post('/', function(req, res){
-  if (req.body.todos) {
-    todos.push(req.body.todos)
-  } else if (req.body.done) {
-    for (let i = 0; i < todos.length; i++) {
-      if (todos[i]===req.body.done){
-        todos.splice(i, 1)
+app.get('/', function(req, res) {
+  models.todos.findAll({
+      where: {
+        completed: false
       }
-    }
-    done.push(req.body.done)
-  }
-  res.redirect('/')
+    })
+    .then(function(todos) {
+      models.todos.findAll({
+          where: {
+            completed: true
+          }
+        })
+        .then(function(completed) {
+          res.render('index', {
+            todos: todos,
+            completed: completed
+          })
+        })
+    })
 })
 
-app.listen(3000, function(){
+app.post('/add', function(req, res) {
+  models.todos.create({
+    task: req.body.todos,
+    completed: false
+  }).then(function() {
+    res.redirect('/')
+  })
+})
+
+app.post('/complete', function(req, res) {
+  models.todos.update({
+      completed: true
+    }, {
+      where: {
+        task: req.body.done
+      }
+    })
+    .then(function() {
+      res.redirect('/')
+    })
+})
+
+app.post('/clear', function(req, res) {
+  models.todos.destroy({
+    where: {
+      completed: true
+    }
+  })
+  .then(function() {
+    res.redirect('/')
+  })
+})
+
+// app.post('/', function(req, res){
+//   if (req.body.todos) {
+//     todos.push(req.body.todos)
+//   } else if (req.body.done) {
+//     for (let i = 0; i < todos.length; i++) {
+//       if (todos[i]===req.body.done){
+//         todos.splice(i, 1)
+//       }
+//     }
+//     done.push(req.body.done)
+//   }
+//   res.redirect('/')
+// })
+
+app.listen(3000, function() {
   console.log('Node is running');
 })
